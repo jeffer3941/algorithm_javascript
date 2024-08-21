7360f34afec77a5329fe0c703489ecf5d1c6a57a
Jefferson Ferreira <48527533+jeffer3941@users.noreply.github.com>
2024-08-20 23:06:07 -0300

    feat: finalizacao de criacao de methodos para a classe stack

--- pilhas/stack-array.js
+++ pilhas/stack-array.js
@@ -2,4 +2,28 @@ class Stack {
     constructor() {
         this.items = [];
     }
+
+    push(element) {
+        return this.items.push(element);
+    }
+
+    pop(element) {
+       return this.items.pop(element);
+    }
+
+    peek() {
+        return this.items[this.items.length - 1]
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    size() {
+        return this.items.length
+    }
+
+    clear() {
+        this.items = [];
+    }
 }
